b7f34866d040e29a6213bbc78a37ce0fdfaa80af
Alex <alexcab@gmail.com>
2025-02-12 22:13:59 -0300

    Modificacion login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideNavBarComponent } from './side-nav-bar/side-nav-bar.component';
 import { AuthService } from './services/auth.service';
-import { LoginComponent } from './pantallas/login/login.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     SideNavBarComponent, 
-    LoginComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
